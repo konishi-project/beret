975c0079fda382187839f478ed2457333387f9cb
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-07-24 21:15:59 +0400

    Added auth api router.

--- src/main.py
+++ src/main.py
@@ -5,5 +5,7 @@ app = FastAPI()
 
 # Load routers
 from .modules.user.api import router as user_router
+from .modules.auth.api import router as auth_router
 
 app.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
